0c6cec7e1fafa79755521ccf5073fcc222b81efd
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-12 11:37:03 -0300

    Addition of def main

--- src/system.py
+++ src/system.py
@@ -1,19 +1,25 @@
 from functions import *
 
-apart('Visualizador de Formas Geométricas')
-print('Dados para visualizar o Triângulo:')
-print()
-while True:
-    x = validate_trng('Vértice no eixo X: ')
-    y = validate_trng('Vértice no eixo Y: ')
-    if x == y:
-        break
-    print('\033[31mÉ necessário que os vértices possuam o mesmo valor.\033[m')
-print()
-z = validate_dmd('Dados para visualizar o Losango: ')
-print()
-print('======== Triângulo Retângulo ========')
-print_trng(x)
-print('============ Losango ============ ')
-print_dmd(z)
-apart('Fim do Programa, Volte Sempre!!')
+
+def main():
+    apart('Visualizador de Formas Geométricas')
+    print('Dados para visualizar o Triângulo:')
+    print()
+    while True:
+        x = validate_trng('Vértice no eixo X: ')
+        y = validate_trng('Vértice no eixo Y: ')
+        if x == y:
+            break
+        print('\033[31mÉ necessário que os vértices possuam o mesmo valor.\033[m')
+    print()
+    z = validate_dmd('Dados para visualizar o Losango: ')
+    print()
+    print('======== Triângulo Retângulo ========')
+    print_trng(x)
+    print('============ Losango ============ ')
+    print_dmd(z)
+    apart('Fim do Programa, Volte Sempre!!')
+
+
+if __name__ == '__main__':
+    main()
